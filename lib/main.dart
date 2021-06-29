676107ca7811d5dd17ec80fe87361c7ee3e45fcf
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2021-06-29 21:19:03 +0900

    iOSでも通知が送信できることを確認

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,37 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  debugPrint('Got a message in the background! : ${message.data}');
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   await Firebase.initializeApp();
+
+  // iOSの場合は権限を要求する必要がある: https://firebase.flutter.dev/docs/messaging/usage#requesting-permission-apple--web
+  NotificationSettings settings = await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  debugPrint('User granted permission: ${settings.authorizationStatus}');
+
+  // デバイス固有のtokenを取得する
+  final token = await FirebaseMessaging.instance.getToken();
+
+  debugPrint('User’s token is $token');
+
   runApp(const MyApp());
 }
 
@@ -27,6 +55,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late StreamSubscription<RemoteMessage> sub;
+
+  @override
+  void initState() {
+    super.initState();
+    sub = FirebaseMessaging.onMessage.listen((message) {
+      debugPrint('Got a message in the foreground! : ${message.data}');
+    });
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
